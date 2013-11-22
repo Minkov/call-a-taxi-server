eb24ab659fed9eafc92215a602a854b798eee22a
Doncho Minkov <DonchoMinkov@gmail.com>
2013-11-22 11:34:21 +0200

    added init endpoint
    used to wake up the server

---
+++ TaxiOrNot.RestApi/Controllers/InitController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaxiOrNot.Data;
+
+namespace TaxiOrNot.RestApi.Controllers
+{
+    public class InitController : BaseApiController
+    {
+        public string WakeUpServer()
+        {
+            return this.ExecuteOperationAndHandleException(() =>
+            {
+                try
+                {
+                    var phoneId = this.GetPhoneIdHeaderValue();
+                    var context = new TaxiOrNotDbContext();
+                    this.GetUserByPhoneId(phoneId, context);
+                }
+                return "Server is up and running";
+            });
+        }
+    }
+}
