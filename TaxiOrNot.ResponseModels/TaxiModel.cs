ab34ddc91b1ea115c0633d3043ff7336602b4803
Doncho Minkov <donchominkov@gmail.com>
2014-01-02 09:42:11 +0200

    Fixed bugs

--- TaxiOrNot.ResponseModels/TaxiModel.cs
+++ TaxiOrNot.ResponseModels/TaxiModel.cs
@@ -52,6 +52,25 @@ namespace TaxiOrNot.ResponseModels
 
         public string Name { get; set; }
 
+        public string Telephone { get; set; }
+        public string WebSite { get; set; }
+
+        public decimal DailyKmFare { get; set; }
+
+        public decimal DailyBookingFare { get; set; }
+
+        public decimal DailyInitialFare { get; set; }
+
+        public decimal DailyMinFare { get; set; }
+
+        public decimal NightlyMinFare { get; set; }
+
+        public decimal NightlyKmFare { get; set; }
+
+        public decimal NightlyBookingFare { get; set; }
+
+        public decimal NightlyInitialFare { get; set; }
+
         public CityDetailsModel City { get; set; }
     }
 }
